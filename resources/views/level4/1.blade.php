7f8bbbd78eba6638464640620d99124c7070b8b2
Andres Felipe Zea Acevedo <andres.zea@evertecinc.com>
2024-05-11 15:06:51 -0500

    WIP

---
+++ resources/views/level4/1.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('main')
+
+    <paint-permutations
+        :items="{{json_encode([
+            ['name' => 'Azul', 'type' => 'COLOR', 'content' => 'bg-blue-600', 'hex' => '#2563eb' , 'group' => 'Color'],
+            ['name' => 'Verde', 'type' => 'COLOR', 'content' => 'bg-green-600', 'hex' => '#16a34a' , 'group' => 'Color'],
+            ['name' => 'Rojo', 'type' => 'COLOR', 'content' => 'bg-red-600', 'hex' => '#dc2626' , 'group' => 'Color'],
+            ['name' => 'Borrador', 'type' => 'ERASER', 'content' => asset('/images/items/tools/eraser.png'), 'size' => 'BIG', 'group' => 'Borrador']
+        ])}}"
+        :order_to_resolve="[3,5,6,9,11,12,15,17,18]"
+        :size="[2,2]"
+        :solution="[2,2,3,1,1,2,3,3,1]"
+{{--        :solution="[3,3,3,3,3,3,3,3,3]"--}}
+        :fill_sample="[
+        1,3,2,
+        1,2,3,
+        3,2,1,
+        3,1,2,
+        2,1,3,
+        2,3,1,
+        ]"
+        :level="[{{$levelNumber}},{{$activityNumber}}]"
+        :rotate="false"
+    ></paint-permutations>
+
+@endsection
